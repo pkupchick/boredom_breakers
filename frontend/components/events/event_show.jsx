38372d36e04546a017c2064b8e287f423d017df1
Peter Kupchick <pkupchick@gmail.com>
2020-08-01 18:46:14 -0400

    fixed eventshow somehow

--- frontend/components/events/event_show.jsx
+++ frontend/components/events/event_show.jsx
@@ -1,45 +1,59 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchEvent } from '../../actions/event_actions';
+import { fetchEvents, fetchEvent } from '../../actions/event_actions';
 
 class EventShow extends React.Component {
-    constructor(props) {
-        super(props)
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    componentDidMount() {
-        this.props.fetchEvent(this.props.match.params.eventId)
-    }
+  componentDidMount() {
+    this.props.fetchEvent(this.props.match.params.eventId);
+  }
 
-    eventDisplay() {
-        debugger;
-        return (
-            <div>
-                Jill
-            </div>
-        )
-    }
+  eventDisplay() {
+    return <div>Jill</div>;
+  }
 
-    render() {
-        return(
-        <div>
-            <p>hello</p>
-            {this.eventDisplay()}
-        </div>
-        )
-    }
+  events() {
+    const events = this.props.events;
+    let eventsArray = Object.values(events);
+    eventsArray = eventsArray.map((event, idx) => {
+      return (
+        <>
+          <h3 className="event-date-time">
+            {event.event_start} - {event.event_start_time}
+          </h3>
+          <h3 key={idx} className="event-description">
+            {event.title}
+          </h3>
+          <img src={event.photoUrl} alt="" />
+        </>
+      );
+    });
+    return eventsArray;
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.events.title}
+      </div>
+    );
+  }
 }
 
 const msp = (state) => {
     return {
         errors: state.errors.session,
         currentUser: state.session.currentUser,
-        event: state.event
+        events: state.events
     }
 }
 
 const mdp = (dispatch) => {
     return {
+        fetchEvents: () => dispatch(fetchEvents()),
         fetchEvent: (id) => dispatch(fetchEvent(id))
     }
 }
