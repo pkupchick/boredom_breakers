974cebdf476271c279b957d97f0d63179d680ecd
Peter Kupchick <pkupchick@gmail.com>
2020-08-04 17:01:21 -0400

    homepage is looking real close

--- frontend/components/home/homepage.jsx
+++ frontend/components/home/homepage.jsx
@@ -13,11 +13,49 @@ class HomePage extends React.Component {
     this.props.fetchEvents();
   }
 
+  toStandardTime(militaryTime) {
+    // 12:00
+    militaryTime = militaryTime.split(":");
+    if (militaryTime[0].charAt(0) == 1 && militaryTime[0].charAt(1) > 2) {
+      return (
+        militaryTime[0] -
+        12 +
+        ":" +
+        militaryTime[1] +
+        ":" +
+        militaryTime[2] +
+        " P.M."
+      );
+    } else {
+      return militaryTime.join(":") + " A.M.";
+    }
+  }
+
+  toSTime(time) {
+    const timeArray = time.split(":");
+    const hour = timeArray[0];
+    const min = timeArray[1];
+    let standardHour = null;
+    let meridian = null;
+    if (parseInt(hour) > 12) {
+       standardHour = parseInt(hour) - 12;
+       meridian = "PM";
+    } else {
+       standardHour = parseInt(hour);
+       meridian = "AM"
+    }
+    return `${standardHour}:${min} ${meridian} EDT`
+  }
+
   events() {
     const events = this.props.events;
     let eventsArray = Object.values(events);
     eventsArray = eventsArray.map((event, idx) => {
-      const date = new Date(event.event_start);
+    const date = new Date(event.event_start);
+    let time = event.event_start_time;
+      if (!time) { 
+        time = "default:default"
+      }
       return (
         <div className="individual-event">
           <Link to={`events/${event.id}/`}>
@@ -28,7 +66,7 @@ class HomePage extends React.Component {
           </Link>
           <div className="bottom-card">
             <p className="event-date-time">
-              {date.toDateString()} - {event.event_start_time}
+              {date.toDateString()} - {this.toSTime(time)}
             </p>
             <Link to={`events/${event.id}/`}>
               <h3 key={idx} className="event-description">
